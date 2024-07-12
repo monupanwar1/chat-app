0553c14c5912cfa0175baf42bd7e6ec2ecf29c27
panwarkunal <monupanwar5655@gmail.com>
2024-07-12 16:57:40 +0530

    error removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import Chatbox from "./components/Chatbox";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
-import Loginpage from "./components/Loginpage";
+import Registerpage from "./components/SignUp";
+
 
 
 
@@ -14,7 +15,7 @@ const App = () => {
 
   return (
     <>
-      <Loginpage/>
+      <Registerpage/>
       <Navbar/>
     <div className="h-screen w-full p-10 bg-cover bg-center "  style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1604079628040-94301bb21b91?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")' }}>
       
